85442c0f69313ea66c378d41cab8a50aedd12155
Tanmay sarkar <ec2020026@rjit.ac.in>
2022-12-04 00:00:10 +0530

    Twitter Bot
    A twitter Bot Which retweets tweets on the basis of your configuration

---
+++ credentials.py
@@ -0,0 +1,6 @@
+
+consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
